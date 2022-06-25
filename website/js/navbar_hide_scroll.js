8803763f43430a6f078202b130e26dd832285739
AIBaev20@codingburgas.bg <aibaev20@codingburgas.bg>
2022-06-26 00:31:26 +0300

    Add responsive design for tablet on home page

--- website/js/navbar_hide_scroll.js
+++ website/js/navbar_hide_scroll.js
@@ -3,11 +3,24 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 
     var currentScrollPos = window.pageYOffset;
+    var screenWidth = screen.width;
 
     if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    } else {
-    document.getElementById("navbar").style.top = "-75px";
+        document.getElementById("navbar").style.top = "0";
+    }
+    else {
+        if (screenWidth > 900)
+        {
+            document.getElementById("navbar").style.top = "-75px";
+        }
+        else if (screenWidth >= 500 && screenWidth <= 900)
+        {
+            document.getElementById("navbar").style.top = "-215px";
+        }
+        else
+        {
+            document.getElementById("navbar").style.top = "-200px";
+        }
     }
 
     prevScrollpos = currentScrollPos;
